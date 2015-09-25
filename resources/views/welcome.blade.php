394b57e947a1db272042d26f7ce5e19883fb9704
rafaloos <rafal.pacan@gmail.com>
2015-09-25 18:00:18 +0100

    Pierwsz proba z cofnietym commitem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5 dziala</div>
+                <p>Cofniety commit</p>
             </div>
         </div>
     </body>
